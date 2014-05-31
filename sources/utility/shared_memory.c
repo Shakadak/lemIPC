5f5582ae9ef0762adc6f53ddd01dde6d13d2ce2e
npineau <nofigh@hotmail.fr>
2014-05-31 13:34:29 +0200

    normalized

--- sources/utility/shared_memory.c
+++ sources/utility/shared_memory.c
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/29 18:30:13 by npineau           #+#    #+#             */
-/*   Updated: 2014/05/30 16:54:15 by npineau          ###   ########.fr       */
+/*   Updated: 2014/05/31 13:34:05 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@
 
 static void	init_map(t_env *e)
 {
-	e->id = x_int(-1, shmget(e->key, WIDTH * HEIGHT, 0666 | IPC_CREAT), "shmget");
+	e->id = shmget(e->key, WIDTH * HEIGHT, 0666 | IPC_CREAT);
+	x_int(-1, e->id, "shmget");
 	e->map = x_void((void *)-1, shmat(e->id, NULL, 0), "shmat");
 	ft_bzero(e->map, WIDTH * HEIGHT);
 }
